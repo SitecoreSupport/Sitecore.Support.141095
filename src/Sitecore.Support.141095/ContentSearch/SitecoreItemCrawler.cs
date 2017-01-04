89a7808cc0eecc8d6b9cb8c70ec2f3753d1db65f
Andrey Filchenkov <afi@sitecore.net>
2017-01-04 15:04:15 +0200

    Initial

---
+++ src/Sitecore.Support.141095/ContentSearch/SitecoreItemCrawler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Sitecore.ContentSearch;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.Support.ContentSearch
+{
+  public class SitecoreItemCrawler : Sitecore.ContentSearch.SitecoreItemCrawler
+  {
+    public SitecoreItemCrawler()
+    {
+    }
+
+    public SitecoreItemCrawler(IIndexOperations indexOperations) : base(indexOperations)
+    {
+    }
+
+    protected override IEnumerable<IIndexableUniqueId> GetIndexablesToUpdateOnDelete(IIndexableUniqueId indexableUniqueId)
+    {
+      ItemUri iteratorVariable = indexableUniqueId.Value as ItemUri;
+      using (new SecurityDisabler())
+      {
+        Item item;
+        ItemUri uri = new ItemUri(iteratorVariable.ItemID, iteratorVariable.Language, Version.Latest, iteratorVariable.DatabaseName);
+        using (new WriteCachesDisabler())
+        {
+          item = Sitecore.Data.Database.GetItem(uri);
+        }
+        if ((item != null) && (item.Version.Number < iteratorVariable.Version.Number) && (item.Versions.Count > 0)) // Sitecore.Support.141095
+        {
+          yield return new SitecoreItemUniqueId(item.Uri);
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.141095/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.141095")]
+[assembly: AssemblyProduct("Sitecore.Support.141095")]
+[assembly: ComVisible(false)]
